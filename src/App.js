21a80046e931e1bbcaa7c25096e37a6504173242
unknown <ritikpatel05121999@gmail.com>
2021-08-18 01:56:26 +0530

    feat: finished making music recommendation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const artists = Object.keys(data);
+
+	const [artistSelected, setArtistSelected] = useState('Selena Gomez');
+
+	const handleArtistClick = (artist) => {
+		setArtistSelected(artist);
+	};
+
+	return (
+		<main>
+			<h1>Music Recommender</h1>
+
+			<p>
+				Here are my favourite Artists. Select an artist to see their
+				best songs.
+			</p>
+
+			<nav>
+				<ul className='list-artist'>
+					{artists.map((artist, index) => {
+						console.log(artist);
+						return (
+							<li
+								key={index}
+								onClick={() => handleArtistClick(artist)}>
+								{artist}
+							</li>
+						);
+					})}
+				</ul>
+				{artistSelected in data ? (
+					<div className='suggestions-box'>
+						{data[artistSelected].map((music, index) => {
+							return (
+								<div key={index}>
+									<h2>
+										<span>Name: </span>
+										{music.title}
+									</h2>
+									<p>
+										<span>Artists: </span> {music.singers}
+									</p>
+								</div>
+							);
+						})}
+					</div>
+				) : (
+					<></>
+				)}
+			</nav>
+		</main>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = {
+	'Selena Gomez': [
+		{
+			title: 'Wolves',
+			singers: 'Selena Gomez, Marshmello',
+		},
+		{
+			title: 'Past Life',
+			singers: 'Selena Gomez, Tervor Daniel, Lil Money',
+		},
+	],
+	'Katy Perry': [
+		{
+			title: 'Electric',
+			singers: 'Katy Perry',
+		},
+		{
+			title: 'Never Really Over',
+			singers: 'Katy Perry',
+		},
+	],
+	'Olivia Rodrigo': [
+		{
+			title: 'good 4 u',
+			singers: 'Olivia Rodrigo',
+		},
+		{
+			title: 'deja vu',
+			singers: 'Olivia Rodrigo',
+		},
+	],
+	'Doja Cat': [
+		{
+			title: 'Kiss Me More',
+			singers: 'Doja Cat',
+		},
+		{
+			title: 'You Right',
+			singers: 'Doja Cat',
+		},
+	],
+	'Demi Lovato': [
+		{
+			title: 'Met Him Last Night',
+			singers: 'Demi Lovato, Ariana Grande',
+		},
+		{
+			title: 'What Other People Say',
+			singers: 'Demi Lovato, Sam Fischer',
+		},
+	],
+};
+
+export default data;
